666a3295dfad3789c56f99b2f1679c148b7d2423
josephsdavid <josephsd@smu.edu>
2020-03-15 03:40:50 -0500

    almost done

--- CaseStudy10/Analysis/dj_analys.py
+++ CaseStudy10/Analysis/dj_analys.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from typing import Callable
 from sklearn.impute import SimpleImputer
+from pprint import pprint
 plt.style.use('bmh')
 
 # whip up the boston dataset
@@ -67,6 +68,7 @@ def iterate_stats(features: np.ndarray,
     return {'goodness_of_fit': _stats_dict(r2), 'loss': _stats_dict(mse)}
 
 results_dict['baseline'] = iterate_stats(X,y)
+
 impute_types = ['mean','median']
 miss_props = [1, 5, 10, 20, 33, 50]
 n_rounds = X.shape[-1]*100
@@ -113,3 +115,81 @@ plot_loss()
 
 plot_goodness()
 
+
+## mnar!
+
+def _mnar(arr: np.ndarray, col: int) -> np.ndarray:
+    quant = np.quantile(arr[:, col], 0.75)
+    idx = np.where(arr[:,col] >=  quant)
+    out = arr.copy()
+    out[idx, col] = np.nan
+    na_percs = np.isnan(out).astype(int).sum(0) / out.shape[0]
+    print(f"na_percentages: column {col} missing")
+    pprint(na_percs.tolist())
+    return out
+
+
+mnar_dict = {}
+for imp in impute_types:
+    r2 = []
+    mse = []
+    for i in range(X.shape[-1]):
+        data = _mnar(X, i)
+        data_imputed = SimpleImputer(strategy=imp).fit_transform(data)
+        scores = get_scores(data_imputed, y)
+        r2 += [scores['goodness_of_fit']]
+        mse += [scores['mse']]
+    mnar_dict[imp] = {'goodness_of_fit': _stats_dict(r2), 'loss': _stats_dict(mse)}
+mnar_dict['baseline'] = results_dict['baseline']
+
+r2 = []
+mse = []
+for i in range(X.shape[-1]):
+    data = _mnae(X,i)
+    data_imputed = SimpleImputer(strategy='constant', fill_value=-100)
+    scores = get_scores(data_imputed, y)
+    r2 += [scores['goodness_of_fit']]
+    mse += [scores['mse']]
+mnar_dict['constant'] = {'goodness_of_fit': _stats_dict(r2), 'loss': _stats_dict(mse)}
+
+
+def make_plotter(f_type: str) -> Callable:
+    def plot_generic_stats(results: dict=mnar_dict)-> None:
+        ax = plt.subplot()
+        base = results_dict['baseline'][f_type]
+        ax.barh(base['avg'] )
+        keys = [k for k in results.keys() if k != 'baseline']
+        for k in keys:
+            tmp = results_to_series(results[k], f_type)
+            ax.plot(tmp['x'], tmp['y'], label=k.title())
+            ax.fill_between(tmp['x'], tmp['lower'], tmp['upper'], alpha=0.1, label=f"{k} error".title())
+        ax.legend()
+        ax.set_title(f"{f_type} vs Percentage of Missing Data, by Imputation Method".title().replace("_"," "))
+        ax.set_xlabel('Percentage of Missing Data')
+        ax.set_ylabel(f_type.title().replace("_"," "))
+        plt.show()
+    return plot_generic_stats
+
+
+ax = plt.subplot()
+keys = list(mnar_dict.keys())
+avgs = [mnar_dict[imp]['loss']['avg'] for imp in mnar_dict.keys()]
+stds = [mnar_dict[imp]['loss']['std'] for imp in mnar_dict.keys()]
+for k in range(len(keys)):
+    ax.barh(keys[k], avgs[k], label = keys[k], xerr = stds[k])
+ax.set_title('Loss of missing not at random imputation'.title())
+ax.set_xlabel('Loss')
+ax.set_ylabel('Imputation Method')
+plt.show()
+
+
+ax = plt.subplot()
+keys = list(mnar_dict.keys())
+avgs = [mnar_dict[imp]['goodness_of_fit']['avg'] for imp in mnar_dict.keys()]
+stds = [mnar_dict[imp]['goodness_of_fit']['std'] for imp in mnar_dict.keys()]
+for k in range(len(keys)):
+    ax.barh(keys[k], avgs[k], label = keys[k], xerr = stds[k])
+ax.set_title('Goodness of Fit of missing not at random imputation'.title())
+ax.set_xlabel('Goodness of Fit')
+ax.set_ylabel('Imputation Method')
+plt.show()
